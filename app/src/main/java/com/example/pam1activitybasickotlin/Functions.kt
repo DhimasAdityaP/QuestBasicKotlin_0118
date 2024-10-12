0828ca17b527ca03eaaa2a9fb2f2b11f321963ee
DhimasAdityaP <dimasadit0105@gmail.com>
2024-10-12 16:55:59 +0700

    Commit 9 update Functions

--- app/src/main/java/com/example/pam1activitybasickotlin/Functions.kt
+++ app/src/main/java/com/example/pam1activitybasickotlin/Functions.kt
@@ -1,2 +1,30 @@
 package com.example.pam1activitybasickotlin
 
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+// Named argument
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+// Default parameter value
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang: Int, lebar: Int): Int {
+    return panjang * lebar
+}
