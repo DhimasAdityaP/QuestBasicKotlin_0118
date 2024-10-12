4ab5b54bfbd5a475de4df7b4f878cfac52d8fb71
DhimasAdityaP <dimasadit0105@gmail.com>
2024-10-12 17:02:50 +0700

    Commit 12 update lambdafunction

--- app/src/main/java/com/example/pam1activitybasickotlin/LambdaFunction.kt
+++ app/src/main/java/com/example/pam1activitybasickotlin/LambdaFunction.kt
@@ -1,2 +1,11 @@
 package com.example.pam1activitybasickotlin
 
+// Lambda Functions
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+}
